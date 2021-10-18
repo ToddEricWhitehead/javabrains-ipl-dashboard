34a0bc872c9d31a3179d3fca7a5fcaa6059160c6
Todd Whitehead <toddericwhitehead@gmail.com>
2021-10-18 07:38:16 -0500

    06 Building First API Team Info IPL Dashboard - 16:30

---
+++ src/main/java/io/javabrains/ipldashboard/controller/TeamController.java
@@ -0,0 +1,24 @@
+package io.javabrains.ipldashboard.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.javabrains.ipldashboard.model.Team;
+import io.javabrains.ipldashboard.repository.TeamRepository;
+
+@RestController
+public class TeamController {
+	
+	@Autowired
+	private TeamRepository teamRepository;
+	
+	@GetMapping("/team/{teamName}")
+	public Team getTeam(@PathVariable String teamName) {
+		Team team = this.teamRepository.findByTeamName(teamName);
+		
+		return team;
+	}
+	
+}
