680ee192dd6ed3a0be7dfcfe48ebb11700d72a0e
Todd Whitehead <toddericwhitehead@gmail.com>
2021-11-15 08:17:54 -0600

    Building IPL Dashboard = 09 Adding match API and the Match frontend page - 28:30 - Match Page works - but hardcoded values

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -1,13 +1,31 @@
-import { react } from 'react';
+import { react, useEffect, useState } from 'react';
 import { useParms } from 'react-router-dom';
 import { MatchDetailCard } from '../components/MatchDetailCard.js';
 import { MatchSmallCard } from '../components/MatchSmallCard.js';
 
 export const MatchPage = () => {
 	
+	const [matches, setMatches] = useState([]);
+	const teamName = "Delhi Capitals";
+	
+	useEffect(
+		() => {
+			const fetchMatches = async () => {
+				const response = await fetch(`http://localhost:8080/team/${teamName}/matches?year=2019`);
+				const data = await response.json();
+				setMatches(data);
+			};
+			fetchMatches();
+		}, []
+	
+	);
+	
 	return (
 		<div className="MatchPage">
 			<h1>MatchPage</h1>
+			{
+				matches.map(match => <MatchDetailCard teamName={teamName} match={match} />)
+			}
 		</div>
 	);
 }
--- src/main/java/io/javabrains/ipldashboard/controller/TeamController.java
+++ src/main/java/io/javabrains/ipldashboard/controller/TeamController.java
@@ -1,13 +1,18 @@
 package io.javabrains.ipldashboard.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.javabrains.ipldashboard.model.Match;
 import io.javabrains.ipldashboard.model.Team;
 import io.javabrains.ipldashboard.repository.MatchRepository;
 import io.javabrains.ipldashboard.repository.TeamRepository;
@@ -24,11 +29,20 @@ public class TeamController {
 	
 	@GetMapping("/team/{teamName}")
 	public Team getTeam(@PathVariable String teamName) {
-		Team team = this.teamRepository.findByTeamName(teamName);		
-		
+		Team team = this.teamRepository.findByTeamName(teamName);
 		team.setMatches(matchRepository.findLastestMatchesbyTeam(teamName, 4));
 		
 		return team;
+	}
+	
+	@GetMapping("/team/{teamName}/matches")
+	public List<Match> getMatchesForTeam(@PathVariable String teamName, @RequestParam int year) {
+		LocalDate startDate = LocalDate.of(year, 1, 1);
+		LocalDate endDate = LocalDate.of(year +1, 1, 1);
+//		return this.matchRepository.getByTeam1AndDateBetweenOrTeam2AndDateBetweenOrderByDateDesc(
+//				teamName, startDate, endDate, 
+//				teamName, startDate, endDate);
+		return this.matchRepository.getMatchesByTeamBetweenDates(teamName, startDate, endDate);
 	}
 	
 }
